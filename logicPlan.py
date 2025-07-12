f06b47f6fd571c3c966fc10882f536e2a7ce4dc0
claireyoon58 <cyoon18@berkeley.edu>
2025-07-12 15:34:44 -0700

    Add fake change for branching demo - 2025

--- logicPlan.py
+++ logicPlan.py
@@ -10,6 +10,8 @@
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
 # Student side autograding was added by Brad Miller, Nick Hay, and
 # Pieter Abbeel (pabbeel@cs.berkeley.edu).
+# 
+# FAKE CHANGE FOR BRANCHING DEMO - 2025
 
 
 """
